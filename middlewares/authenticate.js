757b11c5c4b6331743115d3c548aba421d593e00
Alvaro <arodriguezmn@maxam.net>
2020-02-11 17:18:42 +0100

    authentication token by jwt middelware

---
+++ middlewares/authenticate.js
@@ -0,0 +1,36 @@
+'use strict'
+
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var secret = 'Secret key'
+
+exports.ensureAuth = function(req, res, next) {
+  if (!req.headers.authorization) {
+    return res.status(403).send({
+      message: 'the request has not the authorization´s header'
+    })
+  }
+
+  var token = req.headers.authorization.replace(/['"]+/g, '');
+
+  try {
+    var payLoad = jwt.decode(token, secret);
+
+    if (payLoad.exp <= moment().unix()) {
+      return res.status(401).send({
+        message: 'Token is expired'
+      })
+    }
+
+  } catch (ex) {
+    console.log(ex)
+    return res.status(404).send({
+      message: 'Token not valid'
+    })
+  }
+
+  req.user = payLoad;
+
+  next();
+
+};
--- routes/user.js
+++ routes/user.js
@@ -4,10 +4,11 @@
 
 var express = require('express');
 var UserController = require('../controllers/user');
+var md_auth = require('../middlewares/authenticate')
 
 var api = express.Router();
 
-api.get('/testingController', UserController.test);
+api.get('/testingController', md_auth.ensureAuth, UserController.test);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser)
 
