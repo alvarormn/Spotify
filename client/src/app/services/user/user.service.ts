82438a50607516cf6f5c55d69ee24bc203f5431a
Alvaro <arodriguezmn@maxam.net>
2020-03-02 22:03:02 +0100

    user service

---
+++ client/src/app/services/user/user.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Header } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+}
