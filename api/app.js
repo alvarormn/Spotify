ceedccee399c56ef126ed799d35b905e88f7737e
Alvaro <arodriguezmn@maxam.net>
2020-03-11 13:39:18 +0100

    Fix the authorization headers

--- api/app.js
+++ api/app.js
@@ -15,10 +15,10 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 //configurar cabeceras http
-app.use(() => (req, res, next) => {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Header', 'Authorization, X-API, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Method', 'GET, POST, OPTIOS, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIOS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIOS, PUT, DELETE');
 
   next();
