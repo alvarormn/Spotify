f0c32ad4c682a5a0986dee055d52ecae2699096d
Alvaro <arodriguezmn@maxam.net>
2020-03-11 12:27:48 +0100

    global config

--- client/src/app/services/global.ts
+++ client/src/app/services/global.ts
@@ -1,12 +1,19 @@
-export var GLOBAL = {
+/*export var GLOBAL = {
   url :'http://localhost:4200/api',
   ip: '127.0.0.1'
-}
-/*export var GLOBAL = {
+}*/
+export var GLOBAL = {
+  server : 'http://localhost:3977',
   url :function(api){
-    if (api = "user") {
-      return 'http://localhost:4200/api/user'
+    if (api === 'user') {
+      return this.server + '/api/user'
+    } else if (api === 'artist') {
+      return this.server + '/api/artist'
+    } else  if (api === 'album') {
+      return this.server + '/api/album'
+    } else  if (api === 'song') {
+      return this.server + '/api/song'
     }
   },
   ip: '127.0.0.1'
-}*/
+}
