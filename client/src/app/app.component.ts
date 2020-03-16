a2a70608226e32c0b0143c71be75d3ab1e3197b0
Alvaro <arodriguezmn@maxam.net>
2020-03-16 12:34:42 +0100

    LogIn Service call complete

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,11 +10,12 @@ import { User } from './models/user'
   styleUrls: ['./app.component.css'],
   providers: [UserService]
 })
-export class AppComponent  implements OnInit{
+export class AppComponent implements OnInit{
   title = 'Musify';
   user: User;
   identity = true;
   token;
+  errorMessage;
 
   constructor(
     private _userService:UserService
@@ -24,21 +25,62 @@ export class AppComponent  implements OnInit{
 
   //ngOnInit se utuliza para ejecutar este código antes de ejecutar el componente
   ngOnInit(){
-
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+    console.log(this.identity)
+    console.log(this.token)
   }
 
   onSubmit(){
-    this._userService.signup(this.user).subscribe(
-      response => {
-        console.log(response)
+
+    this._userService.signup(this.user, null).subscribe(
+      results => {
+        this.errorMessage= null;
+
+        let identity = results.user;
+        this.identity = identity;
+
+        if (!identity._id) {
+          console.error("El usuario no está correctamente identificado")
+        } else {
+
+          this._userService.signup(this.user, true).subscribe(
+            results => {
+              let token = results.token;
+              this.token = token;
+
+              if (this.token.length <= 0) {
+                console.error("El token no se ha generado")
+              } else {
+                localStorage.setItem('identity', JSON.stringify(identity));
+                localStorage.setItem('token', token);
+                //console.log(identity);
+                //console.log(token);
+              }
+
+            },
+            error => {
+              let errorMessage = <any>error;
+              if (errorMessage != null){
+
+                //console.log(errorMessage.error)
+                this.errorMessage = error;
+                console.log(errorMessage.error.message)
+              }
+            }
+          );
+        }
+
       },
       error => {
         let errorMessage = <any>error;
         if (errorMessage != null){
-          console.error(error)
+
+          //console.log(errorMessage.error)
+          this.errorMessage = error;
+          console.log(errorMessage.error.message)
         }
       }
     );
   }
-
 }
--- client/src/app/services/user/user.service.ts
+++ client/src/app/services/user/user.service.ts
@@ -1,28 +1,49 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse, HttpHeaders  } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
 import { GLOBAL } from '../global'
 
 @Injectable()
 export class UserService {
   url: string;
+  identity;
+  token;
   constructor(private _http: HttpClient) {
-    this.url = GLOBAL.url
+    this.url = GLOBAL.url('user');
   }
 
-  signup(user_to_login, gethash = null){
+  signup(user_to_login, gethash): Observable<any>{
+
     if (gethash != null) {
       user_to_login.gethash = gethash;
     }
     let json = JSON.stringify(user_to_login);
     let params = json;
 
-    let headers = new HttpHeaders({'Content-Type':'application/json'});
+    const headers = new HttpHeaders({'Content-Type':'application/json'});
+    return this._http.post(this.url+'/login', params, {headers: headers});
 
-    return this._http
-    .post(this.url+'user/login', params, {headers: headers})
-    .pipe(map(res => res.json()));
+  }
+
+  getIdentity(){
+    const identity = JSON.parse(localStorage.getItem('identity'));
+    if (identity != undefined) {
+      this.identity = identity;
+    } else {
+      this.identity = null
+    }
+    return this.identity;
+  }
+
+  getToken(){
+    const token = localStorage.getItem('token');
+    if (token != undefined) {
+      this.token = token;
+    } else {
+      this.token = null
+    }
+    return this.token;
   }
 
 }
